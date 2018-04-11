ccf6a1c8834378703fee948afc2bb1da49cea679
Algorithm-Sam <oluwatobisamuel2020@gmail.com>
2018-04-11 12:41:27 +0000

    Pushed my first webpage on HngInterns to github

---
+++ pages/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Implementing Figma Design</title>
+	<link rel="stylesheet" type="text/css" href="../css/style.css">
+</head>
+<body>
+	<div class="wrapper">
+			<div class="customBox">
+				<h1 class="heading">Welcome to the <span>HNG</span> INTERNSHIP PROGRAM</h1>
+				<div class="logoHolder">
+					<img src="../images/logo.jpg" />
+				</div>
+				<div class="timeContainer">
+					Today is <?php echo date("l , d M Y. h:i:s a"); ?>
+				</div>
+			</div>
+	</div>
+</body>
+</html>
